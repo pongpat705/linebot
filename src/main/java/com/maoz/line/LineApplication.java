04ccd7092f5d2a2d8a3879fb6fc15181d51ac582
fhluky <fhluky@gmail.com>
2019-11-16 01:26:18 +0700

    change token

--- src/main/java/com/maoz/line/LineApplication.java
+++ src/main/java/com/maoz/line/LineApplication.java
@@ -1,10 +1,17 @@
 package com.maoz.line;
 
+import com.linecorp.bot.model.event.Event;
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.Message;
+import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.spring.boot.annotation.EventMapping;
+import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@LineMessageHandler
 @SpringBootApplication
-
 public class LineApplication {
 
     public static void main(String[] args) {
@@ -12,4 +19,15 @@ public class LineApplication {
     }
 
 
+    @EventMapping
+    public Message handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
+        System.out.println("event: " + event);
+        final String originalMessageText = event.getMessage().getText();
+        return new TextMessage(originalMessageText);
+    }
+
+    @EventMapping
+    public void handleDefaultMessageEvent(Event event) {
+        System.out.println("event: " + event);
+    }
 }
